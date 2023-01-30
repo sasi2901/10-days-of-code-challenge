972808bc5d21e7b6e29536d3792293d615eb4d25
sasi2901 <101095348+sasi2901@users.noreply.github.com>
2023-01-30 17:44:51 +0530

    Create Day_6.py

---
+++ Day_6.py
@@ -0,0 +1,19 @@
+# ******* DEFINING AND CALLING FUNCTIONS *******
+
+def names():  #DEFINING a function called 'names' using def-keyword 
+    print("I love my India") #Telling the function what to do when called
+    print("Do you?")
+
+names()  #CALLING the function
+#Here Indentation is mandatory - indentation is nothing but a blank or empty space at the beginning of a written or printed line or paragraph.
+
+# ******* WHILE LOOPS *******
+i = 1
+while i < 6:  #Prints the statements until the condition becomes true
+  print(i)
+  i += 1
+
+#NOTE: while loop runs until the condition becomes true. If the confition doesn't become false it will run an infinite loop. 
+# Example:
+while 5>3:
+    print("hi")
